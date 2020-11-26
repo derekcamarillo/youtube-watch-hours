a1cfb9f6973b188ac39a1974fd6ab3f60b4be7ae
topskilldev <me@topskill.dev>
2020-11-26 13:18:55 -0800

    - html wheel

--- resources/views/wheel.blade.php
+++ resources/views/wheel.blade.php
@@ -14,14 +14,20 @@
 
             <div class="lucky-wrapper">
                 <div class="block-content">
-                    <div class="text-center wheel"><img src="images/depositphotos_155813190-stock-illustration-fortune-wheel-icon.jpg" alt=""></div>
+                    <div class="text-center wheel">
+                        <canvas id="canvas" width="600" height="600">
+                            Canvas not supported, use another browser.
+                        </canvas>
+                        <img class="border" src="{{ asset('images/wheel_border.png') }}" width="600" height="600">
+                        <img class="center" src="{{ asset('images/wheel_center.png') }}" width="140" height="140">
+                        <img class="ticker" src="{{ asset('images/wheel_ticker.png') }}" width="60" height="74">
+                    </div>
                     <div class="btn-wrapper text-center">
                         <p>Total Winnings : 6,280 coins and 57 VIP Days</p>
-                        <a href="#" class="btn btn-primary">SKIP NOW</a>
+                        <a href="javascript:void(0)" onclick="startWheel()" class="btn btn-primary">SPIN NOW</a>
                     </div>
                 </div>
             </div>
-
 
             <div class="row black-box">
                 <div class="col-lg-4 mb-4">
@@ -52,3 +58,105 @@
         </div>
     </div>
 @endsection
+
+@push('styles')
+    <style>
+        .wheel .border {
+            position: absolute;
+            left: 50%;
+            margin-left: -300px;
+        }
+        .wheel .center {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            margin-top: -162px;
+            margin-left: -70px;
+        }
+        .wheel .ticker {
+            position: absolute;
+            left: 50%;
+            margin-left: -30px;
+        }
+    </style>
+@endpush
+
+@push('scripts')
+    <script src='{{ asset('js/wheel/Winwheel.js') }}'></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+
+    <script>
+        let theWheel = new Winwheel({
+            'responsive'      : true,
+            'outerRadius'     : 260, // Set outer radius so wheel fits inside the background.
+            'innerRadius'     : 70,         // Make wheel hollow so segments dont go all way to center.
+            'textFontSize'    : 40,         // Set default font size for the segments.
+            'textOrientation' : 'horizontal', // Make text vertial so goes down from the outside of wheel.
+            'textAlignment'   : 'outer',    // Align text to outside of wheel.
+            'numSegments'     : 12,         // Specify number of segments.
+            'segments'        :             // Define segments including colour and text.
+                [                               // font size and text colour overridden on backrupt segments.
+                    {'fillStyle' : '#B92722', 'text' : '30Coin', 'textFillStyle' : '#ffffff'},
+                    {'fillStyle' : '#ffffff', 'text' : '2 VIP', 'textFillStyle' : '#000000'},
+                    {'fillStyle' : '#B92722', 'text' : '10$ ', 'textFillStyle' : '#ffffff'},
+                    {'fillStyle' : '#ffffff', 'text' : 'Lost', 'textFillStyle' : '#000000'},
+                    {'fillStyle' : '#B92722', 'text' : 'Bankrupt', 'textFontSize' : 36, 'textFillStyle' : '#ffffff'},
+                    {'fillStyle' : '#ffffff', 'text' : '450', 'textFillStyle' : '#000000'},
+                    {'fillStyle' : '#B92722', 'text' : '300', 'textFillStyle' : '#ffffff'},
+                    {'fillStyle' : '#ffffff', 'text' : '450', 'textFillStyle' : '#000000'},
+                    {'fillStyle' : '#B92722', 'text' : '300', 'textFillStyle' : '#ffffff'},
+                    {'fillStyle' : '#ffffff', 'text' : '450', 'textFillStyle' : '#000000'},
+                    {'fillStyle' : '#B92722', 'text' : '300', 'textFillStyle' : '#ffffff'},
+                    {'fillStyle' : '#ffffff', 'text' : '450', 'textFillStyle' : '#000000'},
+                ],
+            'animation' :           // Specify the animation to use.
+                {
+                    'type'     : 'spinToStop',
+                    'duration' : 10,
+                    'spins'    : 3,
+                    'callbackSound'    : playSound,   // Function to call when the tick sound is to be triggered.
+                    'callbackFinished' : alertPrize,  // Function to call whent the spinning has stopped.
+                    'soundTrigger'     : 'pin'        // Specify pins are to trigger the sound.
+                },
+            'pins' :                // Turn pins on.
+                {
+                    'number'     : 12,
+                    'fillStyle'  : 'silver',
+                    'outerRadius': 3,
+                    'responsive' : true
+                }
+        });
+
+        // Loads the tick audio sound in to an audio object.
+        let audio = new Audio("{{ asset('tick.mp3') }}");
+
+        // This function is called when the sound is to be played.
+        function playSound()
+        {
+            // Stop and rewind the sound if it already happens to be playing.
+            audio.pause();
+            audio.currentTime = 0;
+
+            // Play the sound.
+            audio.play();
+        }
+
+        // Called when the animation has finished.
+        function alertPrize(indicatedSegment)
+        {
+            // Display different message if win/lose/backrupt.
+            if (indicatedSegment.text == 'LOOSE TURN') {
+                alert('Sorry but you loose a turn.');
+            } else if (indicatedSegment.text == 'BANKRUPT') {
+                alert('Oh no, you have gone BANKRUPT!');
+            } else {
+                alert("You have won " + indicatedSegment.text);
+            }
+        }
+
+        function startWheel() {
+            theWheel.animation.spins = 10;
+            theWheel.startAnimation();
+        }
+    </script>
+@endpush
